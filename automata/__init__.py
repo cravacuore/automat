6082881050a507d8c6ddd1ee4766cc86df10aba2
Cravacuore <cravacuore@hotmail.com>
2015-11-19 21:59:57 -0300

    Simpler __init__ render index

--- automata/__init__.py
+++ automata/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask
-from flask import make_response
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return make_response(open(
-        'automata/templates/index.html').read())
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    pass
 
