3fdfe1285a6915a8b2d5453d6de6cfc48abeefd8
enseidler <enseidler@gmail.com>
2015-11-20 15:43:25 -0500

    Passed test - should create final state

--- automata/models.py
+++ automata/models.py
@@ -4,9 +4,19 @@ class State:
 	def isInitialState(self):
 		return False
 
+	def isFinalState(self):
+		return False
+
 
 class InitialState(State):
 
 	def isInitialState(self):
 		return True
+
+
+class FinalState(State):
+
+	def isFinalState(self):
+		return True
+
 
