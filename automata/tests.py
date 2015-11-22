103e191540681b34948eb5e3b9784c534360d767
enseidler <enseidler@gmail.com>
2015-11-21 20:40:26 -0500

    Design changes - New representation for states

--- automata/tests.py
+++ automata/tests.py
@@ -6,47 +6,39 @@ import unittest
 class ModelTestCase(unittest.TestCase):
 
     def test_should_create_initial_state(self):
-        state = InitialState()
-        self.assertTrue(state.is_initial_state())
-        self.assertFalse(state.is_final_state())
-        self.assertFalse(state.is_neutral_state())
+        state = State(True, False)
+        self.assertTrue(state.is_initial_state)
+        self.assertFalse(state.is_final_state)
 
     def test_should_create_final_state(self):
-        state = FinalState()
-        self.assertTrue(state.is_final_state())
-        self.assertFalse(state.is_initial_state())
-        self.assertFalse(state.is_neutral_state())
-
-    def test_should_create_neutral_state(self):
-        state = NeutralState()
-        self.assertTrue(state.is_neutral_state())
-        self.assertFalse(state.is_final_state())
-        self.assertFalse(state.is_initial_state())
+        state = State(False, True)
+        self.assertTrue(state.is_final_state)
+        self.assertFalse(state.is_initial_state)
 
     def test_should_have_no_transitions_when_is_created(self):
-        state = NeutralState()
+        state = State()
         self.assertTrue(not state.transitions)
 
     def test_should_can_replace_transition_states(self):
-        transition_by_0 = NeutralState()
-        transition_by_1 = FinalState()
-        state = NeutralState()
+        transition_by_0 = State()
+        transition_by_1 = State()
+        state = State()
         state.add_transition('0', transition_by_0)
         state.add_transition('1', transition_by_1)
         self.assertEqual(state.transition_state('0'), transition_by_0)
         self.assertEqual(state.transition_state('1'), transition_by_1)
 
     def test_should_return_false_when_validate_symbol_without_transition(self):
-        state = NeutralState()
+        state = State()
         self.assertFalse(state.validate_symbol('0'))
 
     def test_should_return_true_when_validate_symbol_with_transition(self):
-        state = NeutralState()
-        state.add_transition('0', FinalState())
+        state = State()
+        state.add_transition('0', State(False, True))
         self.assertTrue(state.validate_symbol('0'))
 
     def test_should_can_add_states(self):
-        state = InitialState()
+        state = State()
         automata = Automata()
         automata.add_state(state)
         self.assertTrue(state in automata.states)
@@ -60,12 +52,11 @@ class ModelTestCase(unittest.TestCase):
         with self.assertRaises(Exception):
             automata.validate("0010011")
 
-
     def create_automata(self):
-        state0 = InitialState()
-        state1 = NeutralState()
-        state2 = NeutralState()
-        state3 = FinalState()
+        state0 = State(True, False)
+        state1 = State()
+        state2 = State()
+        state3 = State(False, True)
         state0.add_transition('0', state0)
         state0.add_transition('1', state1)
         state1.add_transition('0', state0)
@@ -77,8 +68,6 @@ class ModelTestCase(unittest.TestCase):
         automata.add_state(state2)
         automata.add_state(state3)
         return automata
-
-
 
 
 class WebTestCase(unittest.TestCase):
