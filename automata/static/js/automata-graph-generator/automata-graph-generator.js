1d674f66551cbeb9bcfaf75892a8869654a51e9b
enseidler <enseidler@gmail.com>
2015-11-23 17:51:14 -0500

    Graph generator working - v2

--- automata/static/js/automata-graph-generator/automata-graph-generator.js
+++ automata/static/js/automata-graph-generator/automata-graph-generator.js
@@ -8,6 +8,7 @@ var draw_graph = function(automata){
     var x = 150;
     var centerY = canvas.height / 2;
     var radius = 20;
+    var height_arrow = 20;
 
     function neighbors(origin, destination){
         return Math.abs(origin.circle.x - destination.circle.x) == 100;
@@ -66,7 +67,7 @@ var draw_graph = function(automata){
         this.ctx.stroke();
     };
 
-    function StraightLine(fromx, fromy, tox, toy, ctx){
+    function StraightLine(fromx, fromy, tox, toy, symbol, ctx){
         if(fromx < tox) {
             this.fromx = fromx+20;
             this.tox = tox-20;
@@ -74,6 +75,7 @@ var draw_graph = function(automata){
             this.fromx = fromx-20;
             this.tox = tox+20;
         }
+        this.symbol = symbol;
         this.fromy = fromy;
         this.toy = toy;
         this.ctx = ctx;
@@ -91,18 +93,25 @@ var draw_graph = function(automata){
         this.ctx.lineTo(this.tox-headlen*Math.cos(angle+Math.PI/6),this.toy-headlen*Math.sin(angle+Math.PI/6));
     };
 
-    function LoopLine(fromx, fromy, ctx){
+    function LoopLine(fromx, fromy, symbol, ctx){
+        this.symbol = symbol;
         this.fromx = fromx + 15;
         this.fromy = fromy - 14;
         this.ctx = ctx;
+        if(this.symbol == '0'){
+            this.symboly = 40;
+        } else {
+            this.symboly = 50;
+        }
     }
 
     LoopLine.prototype.draw = function() {
         var headlen = 10; // length of head in pixels
         var dx = 0;
         var dy = 100;
-        var angle = Math.atan2(dy,dx);
-
+        var angle = Math.atan2(dy,dx);  
+        this.ctx.fillStyle = 'black';
+        this.ctx.fillText(this.symbol, this.fromx - 15, this.fromy - this.symboly);
         this.ctx.moveTo(this.fromx, this.fromy);
         this.ctx.lineTo(this.fromx, this.fromy - 15);
         this.ctx.arc(this.fromx - 15, this.fromy - 15, 15, 0, Math.PI, true);
@@ -113,35 +122,44 @@ var draw_graph = function(automata){
         this.ctx.lineTo(this.fromx - 30-headlen*Math.cos(angle+Math.PI/6),this.fromy -headlen*Math.sin(angle+Math.PI/6));
     };        
 
-    function CurvedLine(fromx, fromy, tox, toy, ctx){
+    function CurvedLine(fromx, fromy, tox, toy, symbol, ctx){
         this.fromx = fromx;
         this.fromy = fromy;
         this.tox = tox;
         this.toy = toy;
+        this.symbol = symbol;
         this.ctx = ctx;
+        if(this.symbol == '0'){
+            this.symboly = 10;
+        } else {
+            this.symboly = 20;
+        }
+        height_arrow = height_arrow + 20;
     }
 
     CurvedLine.prototype.draw = function() {
         var headlen = 10; // length of head in pixels
+        this.ctx.fillStyle = 'black';
+        this.ctx.fillText(this.symbol, this.fromx + 30, this.fromy - this.symboly);
         this.ctx.moveTo(this.fromx, this.fromy + 20);
-        this.ctx.lineTo(this.fromx, this.fromy + 40);
-        this.ctx.lineTo(this.tox, this.toy + 40);
+        this.ctx.lineTo(this.fromx, this.fromy + height_arrow);
+        this.ctx.lineTo(this.tox, this.toy + height_arrow);
         this.ctx.lineTo(this.tox, this.toy + 20);
         this.ctx.lineTo(this.tox - headlen/2, this.toy + 20 + headlen);
         this.ctx.moveTo(this.tox, this.toy + 20);
         this.ctx.lineTo(this.tox + headlen/2, this.toy + 20 + headlen);
     };
 
-    function Transition(origin, destination, ctx){
+    function Transition(origin, destination, symbol, ctx){
         this.origin = origin;
         this.destination = destination;
         this.ctx = ctx;
         if(neighbors(origin, destination)){
-            this.arrow  = new StraightLine(origin.x, centerY, destination.x, centerY, this.ctx);
+            this.arrow  = new StraightLine(origin.x, centerY, destination.x, centerY, symbol, this.ctx);
         } else if(origin == destination){
-            this.arrow = new LoopLine(origin.x, centerY, this.ctx);
+            this.arrow = new LoopLine(origin.x, centerY, symbol, this.ctx);
         } else {
-            this.arrow  = new CurvedLine(origin.x, centerY, destination.x, centerY, this.ctx);
+            this.arrow  = new CurvedLine(origin.x, centerY, destination.x, centerY, symbol, this.ctx);
         }
     }
 
@@ -179,15 +197,15 @@ var draw_graph = function(automata){
         var state;
         for(i = 0; i < states_graphs.length; i++){
             state = states_graphs[i];
-            draw_transition(state, state.transition_by_0);
-            draw_transition(state, state.transition_by_1);
+            draw_transition(state, state.transition_by_0, '0');
+            draw_transition(state, state.transition_by_1, '1');
         }
     }
 
-    function draw_transition(state, transition){
+    function draw_transition(state, transition, symbol){
         if((transition != 'None')){
             var destination = get_state(transition);
-            new Transition(state, destination, ctx).draw();       
+            new Transition(state, destination, symbol, ctx).draw();       
         }
     };
 
